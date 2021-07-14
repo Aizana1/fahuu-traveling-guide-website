62e30f433aa80a26fac54b7d3462cfd1c82265f2
Aizana <ayzana2010@gmail.com>
2021-07-15 01:20:59 +0300

    name changed

---
+++ build/precache-manifest.790769842484e592859b73ac76bb0535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e30c47e12f08cce58e80a034865c712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41cdbd8e6e5fa648c11f",
+    "url": "/static/css/main.4eb3524a.chunk.css"
+  },
+  {
+    "revision": "eef42c86445f9d2afe4f",
+    "url": "/static/js/2.175e9834.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.175e9834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cdbd8e6e5fa648c11f",
+    "url": "/static/js/main.f0f96bac.chunk.js"
+  },
+  {
+    "revision": "6854da41de89acf78672",
+    "url": "/static/js/runtime-main.09159183.js"
+  }
+]);
